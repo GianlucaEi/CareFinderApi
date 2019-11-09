96d483a9296df15ab548708603acf26335891750
Bryan Slayton <bryan.slayton1985@gmail.com>
2019-11-09 13:28:29 -0600

    user model - done; working on hospital model

---
+++ src/middleware/error-messages.js
@@ -0,0 +1,12 @@
+const error = {
+    MISSING_FIRSTNAME: 'Missing the required property: [contactInfo: firstName]',
+    MISSING_LASTNAME: 'Missing the required property: [contactInfo: lastName]',
+    MISSING_EMAIL: 'Missing the required property: [email address]',
+    MISSING_PASSWORD: 'Missing the required property: [password]',
+};
+
+const success = {
+
+};
+
+module.exports = {error, success};
