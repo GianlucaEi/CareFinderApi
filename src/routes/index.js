a7ccb03c05555f7cfe6bccb80e104b8240663c00
Gianluca Eickenberg <gianluca@eickenberg.us>
2019-11-09 15:04:36 -0600

    changed route from auth to users

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,9 @@ let express = require('express');
 let router = express.Router();
 
 let hospitalRoutes = require('./hospital-routes');
-let authRoutes = require('./user-routes');
+let userRoutes = require('./user-routes');
 
 router.use('/hospitals', hospitalRoutes);
-router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
