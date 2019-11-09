90ffabb7b48d50188cedaca69eb0be1e7549cf4b
Gianluca Eickenberg <gianluca@eickenberg.us>
2019-11-09 12:20:54 -0600

    added basic routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var hospitalRoutes = require('./hospital-routes');
+var authRoutes = require('./user-routes');
+
+router.use('/hospitals', hospitalRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
